0cc01e93d71aeb199f6412f9374f4a478fe025f9
sivahari23 <sivahari0123@gmail.com>
2026-03-23 12:52:47 +0530

    url changed

--- docs/server/angular-app-manifest.mjs
+++ docs/server/angular-app-manifest.mjs
@@ -2,7 +2,7 @@
 export default {
   bootstrap: () => import('./main.server.mjs').then(m => m.default),
   inlineCriticalCss: true,
-  baseHref: 'C:/Users/s.g.mathivanan/AppData/Local/Programs/Git/siva-portfolio',
+  baseHref: 'C:/Users/s.g.mathivanan/AppData/Local/Programs/Git/siva-portfolio/',
   locale: undefined,
   routes: [
   {
@@ -32,8 +32,8 @@ export default {
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 502, hash: '5df6a00e0036694015f47b732fd5ccae6950c82d8a827700a111f7eeb194eab2', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 1015, hash: 'b38fb46cdd686499c19d557d2831cd39eebb3bbe9b4a98b821955e5f37789722', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 503, hash: 'fa12e5cb2bd2d7575acd775efb1aa8b364e3f807f1aea695eca1d0a49b065945', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 1016, hash: '9fca3a01413dd61e722b198562a65d3fb402605f313ae88176512c9a49e9c0ab', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
     'styles-5INURTSO.css': {size: 0, hash: 'menYUTfbRu8', text: () => import('./assets-chunks/styles-5INURTSO_css.mjs').then(m => m.default)}
   },
 };
